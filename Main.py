699c4c48f5b1068096bc61981e74bba5b72c6832
Kris <krystian.rytel@gmail.com>
2019-10-14 20:59:26 +0200

    iterations

--- Main.py
+++ Main.py
@@ -7,21 +7,35 @@ machinesTotalPayouts = []
 totalPayout = 0
 averagePayouts = []
 machineRunCounts = []
-iterationsCount = 100
+maxIterationsCount = 1000
 eps = 0.1
 
 explorationRatio = 0.2
-explorationIterations = iterationsCount * explorationRatio
 
 def createMachines():
     for i in range(machinesCount):
         reward = random.randint(1, 101)
         rewardProbability = random.uniform(0, 1)
         machines.append(Machine(reward, rewardProbability))
+    createMachineProperties()
+
+def createMachineProperties():
+    for i in range(machinesCount):
         machinesTotalPayouts.append(0)
         averagePayouts.append(0)
         machineRunCounts.append(0)
 
+def resetMachineProperties():
+    global machinesTotalPayouts
+    global totalPayout
+    global averagePayouts
+    global machineRunCounts
+
+    machinesTotalPayouts = []
+    totalPayout = 0
+    averagePayouts = []
+    machineRunCounts = []
+    createMachineProperties()
 
 def printMachines():
     for idx, machine in enumerate(machines):
@@ -47,8 +61,8 @@ def play(index, iteration):
         totalPayout += machines[index].reward
 
     averagePayouts[index] = machinesTotalPayouts[index] / machineRunCounts[index]
-    print(f"Iteracja: {iteration} [{' '.join(str(x) for x in averagePayouts)}]")
-    print(f"Total payout: {totalPayout}")
+    # print(f"Iteracja: {iteration} [{' '.join(str(x) for x in averagePayouts)}]")
+    # print(f"Total payout: {totalPayout}")
 
 
 if __name__ == '__main__':
@@ -57,16 +71,20 @@ if __name__ == '__main__':
     createMachines()
     printMachines()
 
-    for i in range(iterationsCount + 1):
-        if i < explorationIterations:
-            index = getRandomMachineIndex()
-            play(index, i)
-        else:
-            explorationProbability = random.uniform(0, 1)
-
-            if(explorationProbability > eps):
-                index = getHighestPayoutMachineIndex()
-            else:
+    for currentMaxIterations in range(1, maxIterationsCount + 1):
+        explorationIterations = currentMaxIterations * explorationRatio
+        for i in range(currentMaxIterations + 1):
+            if i < explorationIterations:
                 index = getRandomMachineIndex()
-            play(index, i)
-    print(f"Average payout: {totalPayout / iterationsCount}")
+                play(index, i)
+            else:
+                explorationProbability = random.uniform(0, 1)
+
+                if(explorationProbability > eps):
+                    index = getHighestPayoutMachineIndex()
+                else:
+                    index = getRandomMachineIndex()
+                play(index, i)
+        print(f"Average payout: {totalPayout / currentMaxIterations} Max iterations {currentMaxIterations}")
+        resetMachineProperties()
+
